03bc6218404b1711f188902a017c4b8b48bf00ff
pujatB <76119090+pujatB@users.noreply.github.com>
2021-10-04 15:11:51 +0100

    fixed the issue-function not defined

--- static/js/loginAuth.js
+++ static/js/loginAuth.js
@@ -1,36 +1,71 @@
 async function requestLogin(e) {
 	e.preventDefault();
-	try {
-		const options = {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({
-				username: e.target.username.value,
-				password: e.target.password.value
-			})
-		};
-		const r = await fetch(
-			`http://localhost:3000/auth`,
-			options
-		);
-		const data = await r.json();
-		console.log("data", data);
-		if (!data.success) {
-			throw new Error("Login not authorised");
-		}
-		login(data.token);
-	} catch (err) {
-		console.warn(err);
-	}
+	console.log("hello")
+	console.log(e.target.usernameLogin.value)
+	console.log(e.target.passwordLogin.value)
+// 	try {
+// 		const options = {
+// 			method: "POST",
+// 			headers: { "Content-Type": "application/json" },
+// 			body: JSON.stringify({
+// 				username: e.target.usernameLogin.value,
+// 				password: e.target.passwordLogin.value
+// 			})
+// 		};
+// 		const r = await fetch(
+// 			`http://localhost:3000/auth`,
+// 			options
+// 		);
+// 		const data = await r.json();
+// 		console.log("data", data);
+// 		if (!data.success) {
+// 			throw new Error("Login not authorised");
+// 		}
+// 		login(data.token);
+// 	} catch (err) {
+// 		console.warn(err);
+// 	}
+// }
+
+// function login(token) {
+// 	const user = jwt_decode(token);
+// 	localStorage.setItem("token", token);
+// 	localStorage.setItem("username", user.username);
+// 	localStorage.setItem("id", user.id);
+// 	window.location.href = "../html/dashboard.html";
 }
 
-function login(token) {
-	const user = jwt_decode(token);
-	localStorage.setItem("token", token);
-	localStorage.setItem("username", user.username);
-	localStorage.setItem("id", user.id);
-	window.location.href = "../html/dashboard.html";
+
+async function requestRegistration(e) {
+	e.preventDefault();
+	console.log("hello")	
+	console.log(e.target.usernameRegister.value);
+	console.log(e.target.passwordRegister.value);
+	// try {
+	// 	const options = {
+	// 		method: "POST",
+	// 		headers: { "Content-Type": "application/json" },
+	// 		body: JSON.stringify({
+	// 			username: e.target.usernameRegister.value,
+	// 			password: e.target.passwordRegister.value
+	// 		})
+	// 	};
+		
+	// 	const r = await fetch(
+	// 		`https://localhost:3000/auth/register`,
+	// 		options
+	// 	);
+	// 	const data = await r.json();
+	// 	if (data.err) {
+	// 		throw Error(data.err);
+	// 		return;
+	// 	}
+	// 	window.location.href = "../html/index.html";
+	// } catch (err) {
+	// 	console.warn(err);
+	// }
 }
 
 
-module.exports = requestLogin ;
+module.exports = {requestLogin: requestLogin,
+requestRegistration:requestRegistration};
--- static/js/script.js
+++ static/js/script.js
@@ -1,57 +1,73 @@
+const { requestRegistration, requestLogin } = require("./loginAuth");
+
 const options = {
-    linkSelector: 'a',
-    debugMode: true,
+  linkSelector: "a",
+  debugMode: true,
 };
 
 //animation library
 var swup = new Swup(options);
 
+//Eventlisteners on submit buttons
+window.addEventListener("load", () => {
+  const loginForm = document.getElementById("loginForm");  
+  const registerForm = document.getElementById("registrationForm");
+  if (loginForm) {
+    loginForm.addEventListener("submit",requestLogin);
+  }
+
+  if (registerForm){
+  registerForm.addEventListener("submit", (e)=>{
+    console.log("e" + e);  
+    const pass = passwordMatch();
+    if (pass){ 
+        console.log("hello hi")
+        requestRegistration(e);
+    }
+    else {
+        e.preventDefault(); 
+        passwordMatch(e);
+    }
+    
+  })
+  }
+})
 
 // Hiding the registration form when the page loads
-document.addEventListener('DOMContenLoaded', (e) => {
-    e.preventDefault();
-    hideRegistrationForm();
+document.addEventListener("DOMContenLoaded", (e) => {
+  e.preventDefault();
+  hideRegistrationForm();
 });
 
-
-//Validations for the input fields
-const usernameRegister = document.getElementById("usernameRegister")
-const passwordRegister = document.getElementById("passwordRegister")
-const confirmPassword = document.getElementById("confirmPassword")
+//Validations for the confirm passsword
+const usernameRegister = document.getElementById("usernameRegister");
+const passwordRegister = document.getElementById("passwordRegister");
+const confirmPassword = document.getElementById("confirmPassword");
 const popUp = document.getElementById("passwordPopup");
+const registerSubmit = document.getElementById('registrationSubmit')
 
-confirmPassword.addEventListener("blur", (e) => {
+if (confirmPassword) {
+    confirmPassword.addEventListener("blur", (e) => {
     passwordMatch();
-})
+  });
+}
 
 
 function passwordMatch() {
-    console.log(passwordRegister.value)
-    console.log(confirmPassword.value)
-    if (passwordRegister.value !== confirmPassword.value) {
-        popUp.classList.toggle("show")
-        confirmPassword.focus();
-    }
+  console.log(passwordRegister.value);
+  console.log(confirmPassword.value);
+  if (passwordRegister.value !== confirmPassword.value) {
+    popUp.classList.toggle("show");
+    confirmPassword.focus();
+    return false;
+  }
+  else {
+      return true;
+  }
 }
 function hideRegistrationForm() {
-    registerForm.classList.add('hideForm')
+  registerForm.classList.add("hideForm");
 }
-
-
-//Eventlisteners on submit buttons
-window.addEventListener("load",()=> {
-    const loginForm = document.getElementById('loginForm')
-    const registerForm = document.getElementById('registrationForm')
-    loginForm.addEventListener('submit', requestLogin);
-    registerForm.addEventListener('submit', requestRegistration);
-})
-
-
-
-
-
-
-
 
 // Shows the registration form when the sign up link is clicked
 // const signUp = document.querySelector('a')
@@ -63,3 +79,5 @@ window.addEventListener("load",()=> {
 //     registerForm.classList.remove('hideForm')
 //     loginForm.classList.add('hideForm')
 // }
+
+module.exports = passwordMatch;
