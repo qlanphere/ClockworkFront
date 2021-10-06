0983cddccbe82e8f5b9f194538b86046c03998d7
Quinn Lanphere <qlanphere@yahoo.com>
2021-10-06 13:56:21 +0100

    post and get now working with heroku, added headers

--- bundle.js
+++ bundle.js
@@ -3,8 +3,137 @@
 
 
 },{}],2:[function(require,module,exports){
-const jwt_decode = require('jwt-decode');
+const currentId = localStorage.getItem('id')
+console.log(currentId);
+const host = 'clockworkback.herokuapp.com'//'localhost'
+//const port = 3000
+const submitButton = document.getElementById('habitSubmit')
+submitButton.addEventListener('click', postHabit)
 
+const bronze = "../badges/Bronze.png"
+const silver = "../badges/Silver.png"
+const gold = "../badges/Gold.png"
+
+const showForm = document.getElementById('add-habit')
+showForm.addEventListener('click', show)
+
+function show() {
+    console.log('clicked')
+    document.getElementById('habitAddPage').classList.toggle('active')
+}
+
+const checkPositive = document.getElementById('positive')
+const frequency = document.querySelector('.frequency')
+
+checkPositive.addEventListener('click', getHabits)
+
+function hide() {
+    frequency.classList.toggle('hidden')
+
+}
+
+function postHabit(e) {
+    e.preventDefault();
+    let habitName = document.getElementById('habitName').value
+    let frequency = document.getElementById('frequency').value
+    let targetDate = document.getElementById('targetDate').value
+    let negative = document.getElementById('negative')
+    let negValue 
+
+    console.log(negative.checked)
+    
+    if(negative.checked) {
+        negValue = false 
+    } else {
+        negValue = true
+    }
+
+        
+    const habitData = {
+        habitName: habitName,
+        frequency: frequency,
+        targetDate: targetDate,
+        habitType: negValue,
+        userId: currentId
+    }
+    console.log(habitData)
+    const url = `https://${host}/habits/`
+    const options = {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            
+            "authorization":localStorage.getItem('token')
+        },
+        body: JSON.stringify(habitData)
+    }
+    console.log(options.body)
+    fetch(url, options)
+}
+
+async function getHabits(e) {
+
+    e.preventDefault()
+
+    let url = `https://${host}/habits/user/${currentId}`
+    let options = {
+        method: "GET",
+        mode: 'cors',
+        headers: { "Content-Type": "application/json",
+                    "authorization": localStorage.getItem('token')
+                }
+    }
+
+    fetch(url,options)
+    .then(r => r.json())
+
+    .then(data => {
+        console.log(data)
+        for(let i=0;i<data.length;i++) {
+
+            let habitId = data[i].habitId
+            let habitName = data[i].habitName
+            let frequency = data[i].frequency
+            let startDate = data[i].startDate
+            let targetDate = data[i].targetDate
+            let habitType = data[i].habitType
+
+           displayHabits(habitId, habitName, frequency, startDate, targetDate, habitType)
+        }
+    })
+}
+
+function displayHabits(habitId, habitName, frequency, startDate, targetDate, habitType) {
+    const habitBox = document.getElementById('habit-container')
+    const newHabit = document.createElement('div')
+    const habitTitle = document.createTextNode(habitName)
+    const habitStart = document.createTextNode
+    habitBox.appendChild(newHabit)
+    newHabit.appendChild(habitTitle)
+
+    
+}
+
+function badgeChecker(badgePoints) {
+    let badge
+    if (badgePoints > 50) {
+        badge = bronze
+    } if (badgePoints > 100) {
+        badge = silver
+    } if (badgePoints > 150) {
+        badge=gold
+    }
+    else badge = ""
+    return badge
+}
+
+module.exports = { badgeChecker, displayHabits, getHabits, postHabit, show, hide}
+
+
+
+},{}],3:[function(require,module,exports){
+const jwt_decode = require('jwt-decode');
+host = 'clockworkback.herokuapp.com'
 async function requestLogin(e) {
     e.preventDefault();
     // console.log("hello")
@@ -21,7 +150,7 @@ async function requestLogin(e) {
       };
       console.log('i made it')
       const r = await fetch(
-        `https://clockworkback.herokuapp.com/auth/login/`,
+        `https://${host}/auth/login/`,
         options
       );
       console.log('im here too')
@@ -63,7 +192,7 @@ async function requestLogin(e) {
 		};
 		
 		const r = await fetch(
-			`https://clockworkback.herokuapp.com/auth/register/`,
+			`https://${host}/auth/register/`,
 			options
 		);
 		const data = await r.json();
@@ -79,7 +208,7 @@ async function requestLogin(e) {
 
 module.exports = {requestLogin: requestLogin, requestRegistration: requestRegistration};
 
-},{"jwt-decode":1}],3:[function(require,module,exports){
+},{"jwt-decode":1}],4:[function(require,module,exports){
 // async function requestRegistration(e) {
 // 	e.preventDefault();
 // 	// console.log("hello")	
@@ -111,7 +240,7 @@ module.exports = {requestLogin: requestLogin, requestRegistration: requestRegist
 
 
 // module.exports = {requestRegistration:requestRegistration};
-},{}],4:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 const { requestLogin, requestRegistration } = require("./loginAuth");
 
 const options = {
@@ -120,7 +249,7 @@ const options = {
 };
 
 //animation library
-var swup = new Swup(options);
+//var swup = new Swup(options);
 
 //Eventlisteners on submit buttons
 window.addEventListener("load", () => {
@@ -196,4 +325,4 @@ function hideRegistrationForm() {
 // }
 
 module.exports = passwordMatch;
-},{"./loginAuth":2}]},{},[4,2,3]);
+},{"./loginAuth":3}]},{},[5,3,4,2]);
